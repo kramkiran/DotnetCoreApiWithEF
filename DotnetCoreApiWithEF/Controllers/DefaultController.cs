e44ba3a596a4ab100c4a2a02612a6e6e2421c224
kramkiran <kramkiran@QUAL-LTKRAM>
2021-03-02 18:32:20 +0530

    web api controller added & configured

---
+++ DotnetCoreApiWithEF/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreApiWithEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [Route("getString")]
+        [HttpGet]
+        public string show()
+        {
+            return "abc";
+        }
+    }
+}
