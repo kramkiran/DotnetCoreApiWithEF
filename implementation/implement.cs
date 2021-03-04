5356136be4e78fcd9db605193750149671a6d5dc
kramkiran <kramkiran@QUAL-LTKRAM>
2021-03-04 12:21:58 +0530

    added interfaces & Implementtion librarys

---
+++ implementation/implement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using interfaces;
+
+namespace implementations
+{
+    public class implement:interfaceType
+    {
+        public string getStringvalue()
+        {
+            return "some value";
+        }
+    }
+}
